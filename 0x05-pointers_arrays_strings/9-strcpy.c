896edd517b09e209494798b3cc0afb0fcc43d51b
Cynthia-00 <cynthiaejike00@gmail.com>
2022-09-19 22:52:25 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,11 +14,11 @@ char *_strcpy(char *dest, char *src)
 
 	while (*(src + inc) != '\0')
 	{
-		*(dest * inc) = *(src = inc);
-		inc++;
+		*(dest + inc) = *(src + inc);
+		inc ++;
 	}
 	*(dest + inc) = '\0';
 
-
 	return (dest);
+
 }
