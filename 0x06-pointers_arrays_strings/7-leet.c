07cfcdedfd1a3326978cc93e39d556a0e95f98e5
Cynthia-00 <cynthiaejike00@gmail.com>
2022-09-22 21:25:29 -0700

    7-leet.cc

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -28,8 +28,8 @@ char *leet(char *str)
 
 char transform(char x)
 {
-	char mapping_low[8] = {'o', 'l', '\0', 'e', 'a', '\0',
-	char mapping_upper[8] = {'O', 'L', '\0', 'E', 'A', '\0'
+	char mapping_low[8] = {'o', 'l', '\0', 'e', 'a', '\0', 't'};
+	char mapping_upper[8] = {'O', 'L', '\0', 'E', 'A', '\0', 'T'};
 	int i = 0;
 	char replacement = x;
 
